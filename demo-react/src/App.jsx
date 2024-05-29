2958268831d3ed7014ab220042fd56337daf02f8
Jang <jang1902er@gmail.com>
2024-05-29 09:49:13 +0700

    Add ex1

--- demo-react/src/App.jsx
+++ demo-react/src/App.jsx
@@ -1,20 +1,47 @@
-import { useState } from 'react'
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-const App = () => {
-  const greeting = 'test';
+import React, { useState } from 'react';
+
+const employeeCodes = ['NV001', 'NV002', 'NV003', 'NV004', 'NV005', 'NV006', 'NV007', 'NV008', 'NV009', 'NV010'];
+
+function App() {
+  let [newCode, setNewCode] = useState('');
+  let [message, setMessage] = useState('');
+
+  const handleInputChange = (e) => {
+    setNewCode(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!newCode) {
+      setMessage('Mã nhân viên không được để trống');
+      return;
+    }
+
+    if (employeeCodes.includes(newCode)) {
+      setMessage('Mã nhân viên đã tồn tại trong hệ thống');
+      return;
+    }
+
+    employeeCodes.push(newCode);
+    setMessage('Thêm mã nhân viên thành công');
+    setNewCode('');
+  };
 
   return (
     <div>
-      <Welcome text={greeting} />
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={newCode} onChange={handleInputChange} />
+        <button type="submit">Thêm mã nhân viên</button>
+      </form>
+      <p>{message}</p>
+      <p>{employeeCodes.join(',')}</p>
     </div>
   );
-};
-
-const Welcome = (props) => {
-  console.log(Welcome);
-  return <h1>{props.text}</h1>;
-};
+}
 export default App
